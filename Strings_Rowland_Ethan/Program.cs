a1b7fb1779026308e673808c47dc45971b7ec5e6
generalrowland <ethandale0916@gmail.com>
2023-03-02 14:48:13 -0600

    upd

--- Strings_Rowland_Ethan/Program.cs
+++ Strings_Rowland_Ethan/Program.cs
@@ -6,7 +6,30 @@ namespace Strings_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Askes user to imput a character and assisngs input to a variable
+            Console.WriteLine("Please input a character");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks if the character user entered is a letter
+            bool isLetter = Char.IsLetter(myChar);
+
+            // Prints the results to console
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}.");
+
+            // Asks user a question and saves response to a variable, sentence
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            // Asks user to enter a word, saves response to a variable called word
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            // Checks if the word is in the sentece
+            bool isIn = sentence.Contains(word);
+
+            // Prints if the word is in the sentence
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
+
         }
     }
 }
